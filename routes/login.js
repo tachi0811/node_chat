1745a36d024d4d150b0b6ea7fbe8061f1655fb7a
developer <developer@TacBookAir.local>
2016-12-04 12:04:38 +0900

    コミット

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(true);
 });
 
 router.post('/', function(req, res, next){
