f1bff370901008ef686c62b14888e1281a78a756
developer <developer@TacBookPro.local>
2016-12-05 21:39:11 +0900

    sequeloze-cli commit

---
+++ db/connect.js
@@ -0,0 +1,2 @@
+var sequelize = require('sequelize');
+var sequelize = new sequelize('dbut7118d16299', 'umrxisczycwgga', 'fJtbYmlAOXFTtpP-llgUC2u2sZ', {host: 'ec2-174-129-223-193.compute-1.amazonaws.com', port: 5432, dialect: 'postgres'});
