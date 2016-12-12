84dfa5bf073d6bf6d9cfea7201553c16915228a6
root <root@localhost.localdomain>
2016-12-12 19:21:18 +0900

    データ登録処理追加

--- routes/login.js
+++ routes/login.js
@@ -28,14 +28,30 @@ router.get('/getUser', function(req, res, next) {
   }
 });
 
-/*
-  
- */
+/* ******************************
+  データ登録
+res.query.email
+res.query.name
+res.query.password
+****************************** */
 router.post('/createUser', function(req, res, next) {
-  //res.contentType("application/JSON");
-  var data = {result : "1", message: "正常終了しました。"};
-  res.send(JSON.stringify(data));
-  
+  res.contentType("application/JSON");
+  try {
+    db.User.findOne({
+      where : {
+        email : req.query.email
+      }
+    })
+    .then(function(data){
+      if (data.length != 0) {
+        res.send({ result : "1", message : "Exist Mail Address"});
+      }
+      
+    });
+  } catch(err) {  
+    var data = {result : "1", message: "DB Access Error"};
+    res.send(JSON.stringify(data));
+  }  
 });
 
 router.post('/', function(req, res, next){
