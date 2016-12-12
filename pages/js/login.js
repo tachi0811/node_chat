284c71b1f8e8e5210db7a23fd9af9c01e688ebcc
root <root@localhost.localdomain>
2016-12-12 16:29:14 +0900

    tuika

--- pages/js/login.js
+++ pages/js/login.js
@@ -2,12 +2,18 @@
 // jquery windows.ready と同じ
 // ******************************
 $(function(){
+  $("#loginform").validationEngine();
 
   // ******************************
   // login button click
   // ******************************
   $("#login").click(function(e){
     
+    // jquery.ValidationEngin
+    if (!$("#loginform").validationEngine('validate')) {
+      return false;
+    }
+
     showLoading();
 
     var sendData = {
@@ -21,21 +27,26 @@ $(function(){
       charset: "UTF-8",
       contentType: "application/JSON",
       url: "/login/getUser",
+      timeout: 3000,
     // ********************
     // 通信成功
     // ********************
     }).done(function(res, status, xhr) {
+      
       $("#messageDialog span").text("success");
       $("#messageDialog").dialog({
         modal: true
-    });
+      });
     // ********************
     // 通信失敗
     // ********************
     }).fail(function(xhr, status, thrown) {
-      aleart("error");
+      $("#messageDialog span").text("error");
+      $("#messageDialog").dialog({
+        modal: true
+      });
     // ********************
-    // 
+    // その他
     // ********************
     }).always(function(xhr, status){
       hideLoading();
--- routes/login.js
+++ routes/login.js
@@ -1,39 +1,35 @@
 var express = require('express');
 var router = express.Router();
-// var models = require('../models');
-
-var sequelize = require('sequelize');
-var sequelize = new sequelize('sampleDB', 'sampleUser', 'password', {host: "localhost", port: "5432", dialect: "postgres"} );
-
-sequelize.sync();
+var db = require('../models');
 
 /* GET users listing. */
 router.get('/getUser', function(req, res, next) {
-  var email = req.query.email;
-  var password = req.query.password;
-  var responseData = {"data": email, "password": password};
-
   res.contentType("application/JSON");
 
-  sequelize.query('select * from \"Users\"', { type: sequelize.QueryTypes.SELECT}).
-  then(function(data){
-    res.send(JSON.stringify(data));
-//    console.log(data);
-  });
-
-  // sequelize.query('insert into sample values select max(id) + 1, \'sample\' from sample').spread(function(result, metadata) {
-// 
-  // });
-  //  
-  // sequelize.query('update sample set name = \'abc\'').spread(function(result, metadata) {
-// 
-  // });
-
-  // res.send(JSON.stringify(responseData));
+  // ====================
+  // 直接SQLを発行する
+  // ====================
+  //  db.sequelize.query('select * from \"Users\"', { type: db.sequelize.QueryTypes.SELECT})
+  //  .then(function(data){
+  //    res.send(JSON.stringify(data));
+  //  });
+  try {
+    db.User.findAll({
+      where : {
+        email : req.query.email,
+        password : req.query.password
+    }})
+    .then(function(data){
+      res.send(data)
+    });
+  } catch (err) {
+    res.send("error");
+    throw(err);
+  }
 });
 
 router.post('/', function(req, res, next){
-  res.send('aaabbbccc');
+  res.send('data');
 });
 
 module.exports = router;
