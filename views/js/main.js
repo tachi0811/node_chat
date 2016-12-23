d6bcbd7e5a10eda47fe11c002452b13abc9909e1
root <root@localhost.localdomain>
2016-12-23 14:25:12 +0900

    [[

--- views/js/main.js
+++ views/js/main.js
@@ -93,7 +93,7 @@ function getChat(id) {
   // 通信成功
   }).done(function(res, status, xhr) {
     if (res.result == "0") {
-      createSideBar(JSON.parse(res.data));
+      createChat(JSON.parse(res.data));
     } else if (res.result == "1") {
       // window.location.href = "./sample.html";
     }
